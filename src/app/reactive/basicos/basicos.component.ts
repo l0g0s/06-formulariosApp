e5c00286b137c2f791fb84d8acf89590340f2be3
alvaro.herrera <alvaro.herrera@euigs.com>
2021-04-20 17:11:05 +0200

    otro

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('RTX 4080ti'),
+  //   precio: new FormControl(1500),
+  //   existencias: new FormControl(3),
+  // })
 
-  ngOnInit(): void {
+  miFormulario: FormGroup = this.fb.group({
+    nombre     : [ , [Validators.required,Validators.minLength(3)] ],
+    precio     : [ , [Validators.required,Validators.min(0)]       ],
+    existencias: [ , [Validators.required,Validators.min(0)]       ],
+  }) 
+
+  constructor( private fb: FormBuilder) { }
+
+  ngOnInit(){
+    this.miFormulario.reset({
+      nombre:'RTX',
+      precio:1600,
+      existencias:10
+    })
+  }
+
+  campoValido(campo:string) {
+    return this.miFormulario.controls[campo].errors
+        && this.miFormulario.controls[campo].touched
+  }
+
+  guardar() {
+    if ( this.miFormulario.invalid ) {
+      this.miFormulario.markAllAsTouched()
+      return
+    }
+    this.miFormulario.reset()
   }
 
 }
--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+interface Persona {
+  nombre:string,
+  favoritos: Favoritos[]
+}
+
+interface Favoritos {
+  id:number,
+  nombre: string
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -8,9 +19,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+  miFormulario:FormGroup = this.fb.group({
+    nombre: [ '', [Validators.required,Validators.minLength(3)]]
+  })
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre:"aaa",
+    favoritos:[{id:1,nombre:"metal"}]
   }
+
+  constructor(private fb:FormBuilder) { }
+  
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre:""
+    })
+  }
+
+  campoValido(campo:string): boolean{
+    return this.miFormulario.controls[campo].invalid
+        && this.miFormulario.controls[campo].touched
+  }
+
+  guardar(){
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched()
+      return
+    }
+    console.log(this.miFormulario.value)
+    this.miFormulario.reset()
+
+  }  
 
 }
